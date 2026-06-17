2afef78e7eb8b7921cd1de545f6a304d3cfdf697
Raulcsilva246 <raulcsilva246@gmail.com>
2026-06-17 15:02:40 -0300

    fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BIN_ID: "SEU_BIN_ID",
-  API_KEY: "SUA_MASTER_KEY"
+  BIN_ID: "6a32c645da38895dfed1576f",
+  API_KEY: "$2a$10$kcSssHws0BKzHxIGtmdqceA65dCJVM9O1Bc9/alBT.S0cCtZWuX/6"
 };
 
--- testes.js
+++
@@ -1,9 +0,0 @@
-const { lerBanco } = require("./jsonbin");
-
-async function teste() {
-  const banco = await lerBanco();
-
-  console.log(banco);
-}
-
-teste();
